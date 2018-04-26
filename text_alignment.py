6fc5a64f59df477fd28be47e76322ef040bb4e49
Erika Green <erikagreen7777@gmail.com>
2018-04-26 19:19:08 +0200

    Update text_alignment.py

--- text_alignment.py
+++ text_alignment.py
@@ -1,73 +1,73 @@
-n Python, a string of text can be aligned left, right and center.
+# n Python, a string of text can be aligned left, right and center.
 
-.ljust(width)
+# .ljust(width)
 
-This method returns a left aligned string of length width.
+# This method returns a left aligned string of length width.
 
->>> width = 20
->>> print 'HackerRank'.ljust(width,'-')
-HackerRank----------  
-.center(width)
+# >>> width = 20
+# >>> print 'HackerRank'.ljust(width,'-')
+# HackerRank----------  
+# .center(width)
 
-This method returns a centered string of length width.
+# This method returns a centered string of length width.
 
->>> width = 20
->>> print 'HackerRank'.center(width,'-')
------HackerRank-----
-.rjust(width)
+# >>> width = 20
+# >>> print 'HackerRank'.center(width,'-')
+# -----HackerRank-----
+# .rjust(width)
 
-This method returns a right aligned string of length width.
+# This method returns a right aligned string of length width.
 
->>> width = 20
->>> print 'HackerRank'.rjust(width,'-')
-----------HackerRank
-Task
+# >>> width = 20
+# >>> print 'HackerRank'.rjust(width,'-')
+# ----------HackerRank
+# Task
 
-You are given a partial code that is used for generating the HackerRank Logo of variable thickness. 
-Your task is to replace the blank (______) with rjust, ljust or center.
+# You are given a partial code that is used for generating the HackerRank Logo of variable thickness. 
+# Your task is to replace the blank (______) with rjust, ljust or center.
 
-Input Format
+# Input Format
 
-A single line containing the thickness value for the logo.
+# A single line containing the thickness value for the logo.
 
-Constraints
+# Constraints
 
-The thickness must be an odd number. 
+# The thickness must be an odd number. 
 
-Output Format
+# Output Format
 
-Output the desired logo.
+# Output the desired logo.
 
-Sample Input
+# Sample Input
 
-5
-Sample Output
+# 5
+# Sample Output
 
-    H    
-   HHH   
-  HHHHH  
- HHHHHHH 
-HHHHHHHHH
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHHHHHHHHHHHHHHHHHHHHHH   
-  HHHHHHHHHHHHHHHHHHHHHHHHH   
-  HHHHHHHHHHHHHHHHHHHHHHHHH   
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-  HHHHH               HHHHH             
-                    HHHHHHHHH 
-                     HHHHHHH  
-                      HHHHH   
-                       HHH    
-                        H 
+#     H    
+#    HHH   
+#   HHHHH  
+#  HHHHHHH 
+# HHHHHHHHH
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#                     HHHHHHHHH 
+#                      HHHHHHH  
+#                       HHHHH   
+#                        HHH    
+#                         H 
 #######################################################################
 
 #Replace all ______ with rjust, ljust or center. 
