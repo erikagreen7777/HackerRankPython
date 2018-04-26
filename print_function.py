71634431d5803a1df947a4199770a3ae949a8fd2
Erika Green <erikagreen7777@gmail.com>
2018-04-26 19:17:58 +0200

    Update print_function.py

--- print_function.py
+++ print_function.py
@@ -1,26 +1,26 @@
-Print Function
+# Print Function
 
-Read an integer .
+# Read an integer .
 
-Without using any string methods, try to print the following:
+# Without using any string methods, try to print the following:
 
 
-Note that "" represents the values in between.
+# Note that "" represents the values in between.
 
-Input Format
+# Input Format
 
-The first line contains an integer .
+# The first line contains an integer .
 
-Output Format
+# Output Format
 
-Output the answer as explained in the task.
+# Output the answer as explained in the task.
 
-Sample Input 0
+# Sample Input 0
 
-3
-Sample Output 0
+# 3
+# Sample Output 0
 
-123
+# 123
 ##################################################################
 from __future__ import print_function
 if __name__ == '__main__':
