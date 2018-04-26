0a5eea3617175111961bc4884e57bd76a2f36c40
Erika Green <erikagreen7777@gmail.com>
2018-04-26 19:17:06 +0200

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,29 +1,29 @@
-Loops
+# Loops
 
-Task 
-Read an integer . For all non-negative integers , print . See the sample for details.
+# Task 
+# Read an integer . For all non-negative integers , print . See the sample for details.
 
-Input Format
+# Input Format
 
-The first and only line contains the integer, .
+# The first and only line contains the integer, .
 
-Constraints
+# Constraints
 
 
-Output Format
+# Output Format
 
-Print  lines, one corresponding to each .
+# Print  lines, one corresponding to each .
 
-Sample Input 0
+# Sample Input 0
 
-5
-Sample Output 0
+# 5
+# Sample Output 0
 
-0
-1
-4
-9
-16
+# 0
+# 1
+# 4
+# 9
+# 16
 #########################################################################
 if __name__ == '__main__':
     n = int(raw_input())
