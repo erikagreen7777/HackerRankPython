ccdb2ebbf0fb05362a2ce8a2c9edd8fd75a91340
Erika Green <erikagreen7777@gmail.com>
2018-04-26 19:16:34 +0200

    Update find_runner_up.py

--- find_runner_up.py
+++ find_runner_up.py
@@ -1,27 +1,27 @@
-Find Runner Up Score
+# Find Runner Up Score
 
-Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given scores. Store them in a list and find the score of the runner-up.
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given scores. Store them in a list and find the score of the runner-up.
 
-Input Format
+# Input Format
 
-The first line contains . The second line contains an array   of  integers each separated by a space.
+# The first line contains . The second line contains an array   of  integers each separated by a space.
 
-Constraints
+# Constraints
 
-Output Format
+# Output Format
 
-Print the runner-up score.
+# Print the runner-up score.
 
-Sample Input 0
+# Sample Input 0
 
-5
-2 3 6 6 5
-Sample Output 0
+# 5
+# 2 3 6 6 5
+# Sample Output 0
 
-5
-Explanation 0
+# 5
+# Explanation 0
 
-Given list is . The maximum score is , second maximum is . Hence, we print  as the runner-up score.
+# Given list is . The maximum score is , second maximum is . Hence, we print  as the runner-up score.
 ################################################################
 if __name__ == '__main__':
     n = int(input())
