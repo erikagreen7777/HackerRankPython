330ecc27f2f18a4ef6906acd2623477ea45d96b9
Erika Green <erikagreen7777@gmail.com>
2018-04-26 19:17:24 +0200

    Update mutations.py

--- mutations.py
+++ mutations.py
@@ -1,58 +1,58 @@
-Mutations
+# Mutations
 
-We have seen that lists are mutable (they can be changed), and tuples are immutable (they cannot be changed).
+# We have seen that lists are mutable (they can be changed), and tuples are immutable (they cannot be changed).
 
-Let's try to understand this with an example.
+# Let's try to understand this with an example.
 
-You are given an immutable string, and you want to make changes to it.
+# You are given an immutable string, and you want to make changes to it.
 
-Example
+# Example
 
->>> string = "abracadabra"
-You can access an index by:
+# >>> string = "abracadabra"
+# You can access an index by:
 
->>> print string[5]
-a
-What if you would like to assign a value?
+# >>> print string[5]
+# a
+# What if you would like to assign a value?
 
->>> string[5] = 'k' 
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-TypeError: 'str' object does not support item assignment
-How would you approach this?
+# >>> string[5] = 'k' 
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'str' object does not support item assignment
+# How would you approach this?
 
-One solution is to convert the string to a list and then change the value.
-Example
+# One solution is to convert the string to a list and then change the value.
+# Example
 
->>> string = "abracadabra"
->>> l = list(string)
->>> l[5] = 'k'
->>> string = ''.join(l)
->>> print string
-abrackdabra
-Another approach is to slice the string and join it back.
-Example
+# >>> string = "abracadabra"
+# >>> l = list(string)
+# >>> l[5] = 'k'
+# >>> string = ''.join(l)
+# >>> print string
+# abrackdabra
+# Another approach is to slice the string and join it back.
+# Example
 
->>> string = string[:5] + "k" + string[6:]
->>> print string
-abrackdabra
-Task 
-Read a given string, change the character at a given index and then print the modified string.
+# >>> string = string[:5] + "k" + string[6:]
+# >>> print string
+# abrackdabra
+# Task 
+# Read a given string, change the character at a given index and then print the modified string.
 
-Input Format 
-The first line contains a string, . 
-The next line contains an integer , denoting the index location and a character  separated by a space.
+# Input Format 
+# The first line contains a string, . 
+# The next line contains an integer , denoting the index location and a character  separated by a space.
 
-Output Format 
-Using any of the methods explained above, replace the character at index  with character .
+# Output Format 
+# Using any of the methods explained above, replace the character at index  with character .
 
-Sample Input
+# Sample Input
 
-abracadabra
-5 k
-Sample Output
+# abracadabra
+# 5 k
+# Sample Output
 
-abrackdabra
+# abrackdabra
 #############################################################
 def mutate_string(string, position, character):
     return string[:position] + character + string[position+1:]
