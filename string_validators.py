bafc417ef985b2a1a01087462f3ee7217c9d3ed4
erikagreen7777 <erikagreen7777@gmail.com>
2018-04-26 17:36:48 +0200

    second

--- string_validators.py
+++ string_validators.py
@@ -1,70 +1,72 @@
-Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+# String Validators
 
-str.isalnum() 
-This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+# Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
 
->>> print 'ab123'.isalnum()
-True
->>> print 'ab123#'.isalnum()
-False
-str.isalpha() 
-This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+# str.isalnum() 
+# This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
 
->>> print 'abcD'.isalpha()
-True
->>> print 'abcd1'.isalpha()
-False
-str.isdigit() 
-This method checks if all the characters of a string are digits (0-9).
+# >>> print 'ab123'.isalnum()
+# True
+# >>> print 'ab123#'.isalnum()
+# False
+# str.isalpha() 
+# This method checks if all the characters of a string are alphabetical (a-z and A-Z).
 
->>> print '1234'.isdigit()
-True
->>> print '123edsd'.isdigit()
-False
-str.islower() 
-This method checks if all the characters of a string are lowercase characters (a-z).
+# >>> print 'abcD'.isalpha()
+# True
+# >>> print 'abcd1'.isalpha()
+# False
+# str.isdigit() 
+# This method checks if all the characters of a string are digits (0-9).
 
->>> print 'abcd123#'.islower()
-True
->>> print 'Abcd123#'.islower()
-False
-str.isupper() 
-This method checks if all the characters of a string are uppercase characters (A-Z).
+# >>> print '1234'.isdigit()
+# True
+# >>> print '123edsd'.isdigit()
+# False
+# str.islower() 
+# This method checks if all the characters of a string are lowercase characters (a-z).
 
->>> print 'ABCD123#'.isupper()
-True
->>> print 'Abcd123#'.isupper()
-False
-Task
+# >>> print 'abcd123#'.islower()
+# True
+# >>> print 'Abcd123#'.islower()
+# False
+# str.isupper() 
+# This method checks if all the characters of a string are uppercase characters (A-Z).
 
-You are given a string . 
-Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+# >>> print 'ABCD123#'.isupper()
+# True
+# >>> print 'Abcd123#'.isupper()
+# False
+# Task
 
-Input Format
+# You are given a string . 
+# Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
 
-A single line containing a string .
+# Input Format
 
-Constraints
+# A single line containing a string .
+
+# Constraints
 
 
-Output Format
+# Output Format
 
-In the first line, print True if  has any alphanumeric characters. Otherwise, print False. 
-In the second line, print True if  has any alphabetical characters. Otherwise, print False. 
-In the third line, print True if  has any digits. Otherwise, print False. 
-In the fourth line, print True if  has any lowercase characters. Otherwise, print False. 
-In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+# In the first line, print True if  has any alphanumeric characters. Otherwise, print False. 
+# In the second line, print True if  has any alphabetical characters. Otherwise, print False. 
+# In the third line, print True if  has any digits. Otherwise, print False. 
+# In the fourth line, print True if  has any lowercase characters. Otherwise, print False. 
+# In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
 
-Sample Input
+# Sample Input
 
-qA2
-Sample Output
+# qA2
+# Sample Output
 
-True
-True
-True
-True
-True
+# True
+# True
+# True
+# True
+# True
 
 #########################################################
 
@@ -76,15 +78,16 @@ if __name__ == '__main__':
             break
         else:
             pass
-        if i == len(s):
+        if i == len(s) - 1:
             print (False)
     for i in range(len(s)):
         if s[i].isalpha():
+            print (s[i])
             print (True)
             break
         else:
             pass
-        if i == len(s):
+        if i == len(s) - 1:
             print (False)
     for i in range(len(s)):
         if s[i].isdigit():
@@ -92,7 +95,7 @@ if __name__ == '__main__':
             break
         else:
             pass
-        if i == len(s):
+        if i == len(s) - 1:
             print (False)
     for i in range(len(s)):
         if s[i].islower():
@@ -100,7 +103,7 @@ if __name__ == '__main__':
             break
         else:
             pass
-        if i == len(s):
+        if i == len(s) - 1:
             print (False)
     for i in range(len(s)):
         if s[i].isupper():
@@ -108,5 +111,5 @@ if __name__ == '__main__':
             break
         else:
             pass
-        if i == len(s):
+        if i == len(s) - 1:
             print (False)
